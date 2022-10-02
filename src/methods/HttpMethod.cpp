1a4014e01cee28c01672bb2e1c01bcd8752dd76f
kkaneko <gorutiru@gmail.com>
2022-10-03 00:20:49 +0900

    create DELETE method(errors are not handled)

--- src/methods/HttpMethod.cpp
+++ src/methods/HttpMethod.cpp
@@ -1,10 +1,13 @@
 #include "../methods/GET.hpp"
+#include "../methods/DELETE.hpp"
 
 HttpMethod* generateHttpMethod( const std::string& name ) {
     HttpMethod* method;
 
     if (name == "GET") {
         method = new GET();
+    } else if (name == "DELETE") {
+        method = new DELETE();
     } else {
         method = NULL;
     }
