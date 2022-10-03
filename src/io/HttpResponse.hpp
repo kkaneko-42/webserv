3c793c3f59087541c9a8dca8693a1695d98ed5dc
kkaneko-window_laptop <gorutiru@gmail.com>
2022-10-03 12:36:08 +0900

    status code PAYLOAD_TOO_LARGE added

--- src/io/HttpResponse.hpp
+++ src/io/HttpResponse.hpp
@@ -18,6 +18,7 @@ class HttpResponse {
             BAD_REQUEST = 400,
             NOT_FOUND = 404,
             METHOD_NOT_ALLOWED,
+            PAYLOAD_TOO_LARGE = 413,
 
             INTERNAL_SERVER_ERROR = 500,
         };
